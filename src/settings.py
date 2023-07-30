f869d6a0eb8935b67d23b50ab79cd53b38d0372b
Ars Vladimirov <grokhael@gmail.com>
2023-07-30 17:21:38 +0600

    upd settings

--- src/settings.py
+++ src/settings.py
@@ -1,45 +1,89 @@
 from typing import Dict, List, Optional, Union
 
-from dataset_tools.templates import AnnotationType, CVTask, Industry, License
+from dataset_tools.templates import (
+    AnnotationType,
+    Category,
+    CVTask,
+    Domain,
+    Industry,
+    License,
+    Research,
+)
 
 ##################################
 # * Before uploading to instance #
 ##################################
-PROJECT_NAME: str = None
-PROJECT_NAME_FULL: str = None
+PROJECT_NAME: str = "COCO 2017 Keypoints"
+PROJECT_NAME_FULL: str = "COCO 2017: Common Objects in Context 2017 (Keypoints)"
 
 ##################################
 # * After uploading to instance ##
 ##################################
-LICENSE: License = None
-INDUSTRIES: List[Industry] = None
-CV_TASKS: List[CVTask] = None
-ANNOTATION_TYPES: List[AnnotationType] = None
+LICENSE: License = License.CC_BY_4_0()
+APPLICATIONS: List[Union[Industry, Domain, Research]] = [Domain.General()]
+CATEGORY: Category = Category.General(benchmark=True)
 
-RELEASE_YEAR: int = None
-HOMEPAGE_URL: str = None
+CV_TASKS: List[CVTask] = [
+    CVTask.InstanceSegmentation(),
+    CVTask.SemanticSegmentation(),
+    CVTask.ObjectDetection(),
+]
+ANNOTATION_TYPES: List[AnnotationType] = [
+    AnnotationType.InstanceSegmentation(),
+    AnnotationType.ObjectDetection(),
+]
+
+RELEASE_DATE: Optional[str] = None  # e.g. "YYYY-MM-DD"
+if RELEASE_DATE is None:
+    RELEASE_YEAR: int = 2017
+
+HOMEPAGE_URL: str = "https://cocodataset.org/#home"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = None
+PREVIEW_IMAGE_ID: int = 1237211
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
-GITHUB_URL: str = None
+GITHUB_URL: str = "https://github.com/dataset-ninja/coco-2017-keypoints"
 # URL to GitHub repo on dataset ninja (e.g. "https://github.com/dataset-ninja/some-dataset")
 
 ##################################
 ### * Optional after uploading ###
 ##################################
-DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = None
+DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = {
+    "2017 Train images [118K/18GB]": "http://images.cocodataset.org/zips/train2017.zip",
+    "2017 Val images [5K/1GB]": "http://images.cocodataset.org/zips/val2017.zip",
+    "2017 Test images [41K/6GB]": "http://images.cocodataset.org/zips/test2017.zip",
+    "2017 Train/Val annotations [241MB]": "http://images.cocodataset.org/annotations/annotations_trainval2017.zip",
+}
 # Optional link for downloading original dataset (e.g. "https://some.com/dataset/download")
 
 CLASS2COLOR: Optional[Dict[str, List[str]]] = None
 # If specific colors for classes are needed, fill this dict (e.g. {"class1": [255, 0, 0], "class2": [0, 255, 0]})
 
-PAPER: Optional[str] = None
+# If you have more than the one paper, put the most relatable link as the first element of the list
+PAPER: Optional[Union[str, List[str]]] = "https://arxiv.org/abs/1405.0312"
 CITATION_URL: Optional[str] = None
-ORGANIZATION_NAME: Optional[Union[str, List[str]]] = None
-ORGANIZATION_URL: Optional[Union[str, List[str]]] = None
-TAGS: List[str] = None
+AUTHORS: Optional[List[str]] = [
+    "Tsung-Yi",
+    "Genevieve Patterson",
+    "Matteo R. Ronchi",
+    "Yin Cui",
+    "Michael Maire",
+    "Serge Belongie",
+    "Lubomir Bourdev",
+    "Ross Girshick",
+    "James Hays Georgia",
+    "Pietro Perona",
+    "Deva Ramanan",
+    "Larry Zitnick",
+    "Piotr Dollár",
+]
+
+ORGANIZATION_NAME: Optional[Union[str, List[str]]] = "COCO Consortium"
+ORGANIZATION_URL: Optional[Union[str, List[str]]] = "https://cocodataset.org/#home"
+
+SLYTAGSPLIT: Optional[Dict[str, List[str]]] = None
+TAGS: Optional[List[str]] = None
 
 ##################################
 ###### ? Checks. Do not edit #####
@@ -53,10 +97,15 @@ def check_names():
 
 
 def get_settings():
+    if RELEASE_DATE is not None:
+        global RELEASE_YEAR
+        RELEASE_YEAR = int(RELEASE_DATE.split("-")[0])
+
     settings = {
         "project_name": PROJECT_NAME,
         "license": LICENSE,
-        "industries": INDUSTRIES,
+        "applications": APPLICATIONS,
+        "category": CATEGORY,
         "cv_tasks": CV_TASKS,
         "annotation_types": ANNOTATION_TYPES,
         "release_year": RELEASE_YEAR,
@@ -68,13 +117,16 @@ def get_settings():
     if any([field is None for field in settings.values()]):
         raise ValueError("Please fill all fields in settings.py after uploading to instance.")
 
+    settings["release_date"] = RELEASE_DATE
     settings["project_name_full"] = PROJECT_NAME_FULL or PROJECT_NAME
     settings["download_original_url"] = DOWNLOAD_ORIGINAL_URL
     settings["class2color"] = CLASS2COLOR
     settings["paper"] = PAPER
     settings["citation_url"] = CITATION_URL
+    settings["authors"] = AUTHORS
     settings["organization_name"] = ORGANIZATION_NAME
     settings["organization_url"] = ORGANIZATION_URL
-    settings["tags"] = TAGS if TAGS is not None else []
+    settings["slytagsplit"] = SLYTAGSPLIT
+    settings["tags"] = TAGS
 
     return settings
